d4420b4fcdc93b5ba60528ec5b622070dd5de3b6
Anandhakrishnan <anandhakrishnanm21@gmail.com>
2026-02-18 15:27:29 +0530

    Activation functions and Loss functions

--- src/ann/activations.py
+++ src/ann/activations.py
@@ -2,3 +2,39 @@
 Activation Functions and Their Derivatives
 Implements: ReLU, Sigmoid, Tanh, Softmax
 """
+
+import numpy as np
+
+def relu(z):
+    """Rectified Linear Unit activation function."""
+    return np.maximum(0, z)
+
+def relu_derivative(z):
+    """Derivative of ReLU activation function."""
+    return np.where(z > 0, 1, 0)
+
+def sigmoid(z):
+    """Sigmoid activation function."""
+    return 1 / (1 + np.exp(-z))
+
+def sigmoid_derivative(z):
+    """Derivative of Sigmoid activation function."""
+    s = sigmoid(z)
+    return s * (1 - s)
+
+def tanh(z):
+    """Hyperbolic Tangent activation function."""
+    return np.tanh(z)
+
+def tanh_derivative(z):
+    """Derivative of Tanh activation function."""
+    t = tanh(z)
+    return 1 - t**2
+
+def softmax(z):
+    """Softmax activation function."""
+    pass
+
+def softmax_derivative(z):
+    """Derivative of Softmax activation function."""
+    pass
--- src/ann/objective_functions.py
+++ src/ann/objective_functions.py
@@ -2,3 +2,25 @@
 Loss/Objective Functions and Their Derivatives
 Implements: Cross-Entropy, Mean Squared Error (MSE)
 """
+
+import numpy as np
+
+def cross_entropy(y_true, y_pred):
+    """Cross-Entropy loss function."""
+    epsilon = 1e-15  # To prevent log(0)
+    y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
+    return -np.sum(y_true * np.log(y_pred)) / y_true.shape[0]
+
+def cross_entropy_derivative(y_true, y_pred):
+    """Derivative of Cross-Entropy loss function."""
+    epsilon = 1e-15  # To prevent division by zero
+    y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
+    return -y_true / y_pred
+
+def mean_squared_error(y_true, y_pred):
+    """Mean Squared Error (MSE) loss function."""
+    return np.mean((y_true - y_pred) ** 2)
+
+def mean_squared_error_derivative(y_true, y_pred):
+    """Derivative of Mean Squared Error (MSE) loss function."""
+    return 2 * (y_pred - y_true) / y_true.shape[0]
