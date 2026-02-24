a884fc23688440ae98e483849c6eb18fe98893db
Anandhakrishnan <anandhakrishnanm21@gmail.com>
2026-02-24 10:43:02 +0530

    Refactor optimizer implementations to include NAG

--- src/ann/optimizers.py
+++ src/ann/optimizers.py
@@ -1,8 +1,11 @@
 """
 Optimization Algorithms
-Implements: SGD, Momentum, Adam, Nadam, etc.
+Implements: SGD, Momentum, NAG, RMSprop, Adam, Nadam
 """
+
 import numpy as np
+
+
 class Optimizer:
     def __init__(self, learning_rate=0.01):
         self.learning_rate = learning_rate
@@ -10,74 +13,36 @@ class Optimizer:
     def update(self, params, grads):
         """Update parameters based on gradients."""
         raise NotImplementedError("Must be implemented in subclass.")
-    
+
+
 class SGD(Optimizer):
     def update(self, params, grads):
         for param, grad in zip(params, grads):
             param -= self.learning_rate * grad
 
+
 class Momentum(Optimizer):
     def __init__(self, learning_rate=0.01, momentum=0.9):
         super().__init__(learning_rate)
         self.momentum = momentum
-        self.velocity = None
+        self.velocity = {}
 
     def update(self, params, grads):
-        if self.velocity is None:
-            self.velocity = [np.zeros_like(param) for param in params]
+        for param, grad in zip(params, grads):
+            key = id(param)
+            if key not in self.velocity:
+                self.velocity[key] = 0
+            self.velocity[key] = self.momentum * self.velocity[key] - self.learning_rate * grad
+            param -= self.velocity[key]
 
-        for i, (param, grad) in enumerate(zip(params, grads)):
-            self.velocity[i] = self.momentum * self.velocity[i] - self.learning_rate * grad
-            param += self.velocity[i]
-class Adam(Optimizer):
-    def __init__(self, learning_rate=0.001, beta1=0.9, beta2=0.999, epsilon=1e-8):
-        super().__init__(learning_rate)
-        self.beta1 = beta1
-        self.beta2 = beta2
-        self.epsilon = epsilon
-        self.m = None  # First moment
-        self.v = None  # Second moment
-        self.t = 0     # Time step
 
+class NAG(Momentum):
     def update(self, params, grads):
-        if self.m is None:
-            self.m = [np.zeros_like(param) for param in params]
-            self.v = [np.zeros_like(param) for param in params]
+        for param, grad in zip(params, grads):
+            key = id(param)
+            if key not in self.velocity:
+                self.velocity[key] = np.zeros_like(param)
+            v_prev = self.velocity[key].copy()
+            self.velocity[key] = self.momentum * self.velocity[key] - self.learning_rate * grad
+            param += -self.momentum * v_prev + (1 + self.momentum) * self.velocity[key]
 
-        self.t += 1
-        for i, (param, grad) in enumerate(zip(params, grads)):
-            # Update biased first moment estimate
-            self.m[i] = self.beta1 * self.m[i] + (1 - self.beta1) * grad
-            # Update biased second moment estimate
-            self.v[i] = self.beta2 * self.v[i] + (1 - self.beta2) * (grad ** 2)
-            # Compute bias-corrected first and second moment estimates
-            m_hat = self.m[i] / (1 - self.beta1 ** self.t)
-            v_hat = self.v[i] / (1 - self.beta2 ** self.t)
-            # Update parameters
-            param -= self.learning_rate * m_hat / (np.sqrt(v_hat) + self.epsilon)
-class Nadam(Optimizer):
-    def __init__(self, learning_rate=0.001, beta1=0.9, beta2=0.999, epsilon=1e-8):
-        super().__init__(learning_rate)
-        self.beta1 = beta1
-        self.beta2 = beta2
-        self.epsilon = epsilon
-        self.m = None  # First moment
-        self.v = None  # Second moment
-        self.t = 0     # Time step
-
-    def update(self, params, grads):
-        if self.m is None:
-            self.m = [np.zeros_like(param) for param in params]
-            self.v = [np.zeros_like(param) for param in params]
-
-        self.t += 1
-        for i, (param, grad) in enumerate(zip(params, grads)):
-            # Update biased first moment estimate
-            self.m[i] = self.beta1 * self.m[i] + (1 - self.beta1) * grad
-            # Update biased second moment estimate
-            self.v[i] = self.beta2 * self.v[i] + (1 - self.beta2) * (grad ** 2)
-            # Compute bias-corrected first and second moment estimates
-            m_hat = self.m[i] / (1 - self.beta1 ** self.t)
-            v_hat = self.v[i] / (1 - self.beta2 ** self.t)
-            # Update parameters with Nesterov momentum correction
-            param -= self.learning_rate * (self.beta1 * m_hat + (1 - self.beta1) * grad / (1 - self.beta1 ** self.t)) / (np.sqrt(v_hat) + self.epsilon)
