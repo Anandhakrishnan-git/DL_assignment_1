ba5524c904a325a8ca393a45074b5e965d9c7cfb
Anandhakrishnan <anandhakrishnanm21@gmail.com>
2026-03-02 17:11:27 +0530

    Refactor objective functions to ensure softmax is applied consistently before loss calculations

--- src/ann/objective_functions.py
+++ src/ann/objective_functions.py
@@ -2,25 +2,31 @@
 Loss/Objective Functions and Their Derivatives
 Implements: Cross-Entropy, Mean Squared Error (MSE)
 """
-
+from activations import softmax
 import numpy as np
 
 def CE_loss(y_true, y_pred):
     """Cross-Entropy loss function."""
+    y_pred  = softmax(y_pred)  # Use softmax to convert logits to probabilities
+    
     epsilon = 1e-15  # To prevent log(0)
     y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
     return -np.sum(y_true * np.log(y_pred)) / y_true.shape[0]
 
 def CE_loss_der(y_true, y_pred):
     """Derivative of Cross-Entropy loss function."""
+    y_pred  = softmax(y_pred)  # Use softmax to convert logits to probabilities
     epsilon = 1e-15  # To prevent division by zero
     y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
     return -y_true / y_pred
 
 def MSE(y_true, y_pred):
     """Mean Squared Error (MSE) loss function."""
+    y_pred  = softmax(y_pred)  # Use softmax to convert logits to probabilities
+
     return np.mean((y_true - y_pred) ** 2)
 
 def MSE_der(y_true, y_pred):
     """Derivative of Mean Squared Error (MSE) loss function."""
+    y_pred  = softmax(y_pred)  # Use softmax to convert logits to probabilities
     return 2 * (y_pred - y_true) / y_true.shape[0]
